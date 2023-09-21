7d3a73eaa7455cdbf897b6ff38e14fb15de6b473
victorsch <victor.c.schiavo@gmail.com>
2023-09-21 01:26:03 +0000

    update

--- recon.sh
+++ recon.sh
@@ -7,4 +7,15 @@ echo Don't drink your $drink_name all at once, we'll be finished shortly.
 
 sleep 2
 
+cat wildcards | assetfinder --subs-only | anew domains
 
+cat domains | httprobe -c 80 --prefer-https | anew hosts
+findomain -f wildcards | tee -a findomain.out
+
+# anew only adds new lines, won't add duplicates
+cat findomain.out | anew domains | httprobe -c 50 | anew hosts
+
+# goes through hosts and saves the body so you can grep through them
+cat hosts | fff -d 1 -S -o roots
+
+# fff made .headers and .body files, headers has the headers of the response and body has the body of the response
