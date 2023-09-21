92391b2c306f005506591e115b21f19ea413cc87
victorsch <victor.c.schiavo@gmail.com>
2023-09-21 01:30:29 +0000

    update

--- debian-setup-env.sh
+++ debian-setup-env.sh
@@ -1,10 +1,11 @@
-apt-get update
+sudo apt-get update
 
-apt-get -y install golang
+sudo apt-get -y install golang
+sudo apt-get -y install binutils
 
 go install github.com/tomnomnom/anew@latest
 go install github.com/tomnomnom/assetfinder@latest
-go install github.com/tomnomnom/fff2latest
+go install github.com/tomnomnom/fff@latest
 go install github.com/tomnomnom/gron@latest
 go install github.com/tomnomnom/gf@latest
 go install github.com/tomnomnom/httprobe@latest
