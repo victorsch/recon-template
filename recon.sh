16cc048842eda99d6fefb64688847ea18d0be1b6
victor <victor.c.schiavo@gmail.com>
2023-09-25 15:50:51 -0400

    updates to recon script

--- recon.sh
+++ recon.sh
@@ -1,9 +1,4 @@
 echo Grab a drink, this may take a while...
-echo What will you be having?
-
-read drink_name
-
-echo Don't drink your $drink_name all at once, we'll be finished shortly.
 
 sleep 2
 
@@ -19,8 +14,7 @@ cat domains | httprobe -c 80 | anew hosts
 cat hosts | fff -d 1 -S -o roots
 
 # fff made .headers and .body files, headers has the headers of the response and body has the body of the response
-cd ..
-aws s3 cp --recursive recon-template/roots s3://mass-recon-warehouse/`basename $PWD`
-aws s3 cp recon-template/hosts s3://mass-recon-warehouse/`basename $PWD`/hosts
-aws s3 cp recon-template/domains s3://mass-recon-warehouse/`basename $PWD`/domains
-aws s3 cp recon-template/wildcards s3://mass-recon-warehouse/`basename $PWD`/wildcards
+aws s3 cp --recursive roots s3://mass-recon-warehouse/`basename $PWD`
+aws s3 cp hosts s3://mass-recon-warehouse/`basename $PWD`/
+aws s3 cp domains s3://mass-recon-warehouse/`basename $PWD`/
+aws s3 cp wildcards s3://mass-recon-warehouse/`basename $PWD`/
