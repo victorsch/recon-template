ec2ddb3e15c55568ae4914cca2dcc205d9f1c9e1
victor <victor.c.schiavo@gmail.com>
2023-09-25 16:03:33 -0400

    new recon script

--- recon.sh
+++ recon.sh
@@ -13,7 +13,16 @@ cat domains | httprobe -c 80 | anew hosts
 # goes through hosts and saves the body so you can grep through them
 cat hosts | fff -d 1 -S -o roots
 
+# get interesting status codes on each host
+# loop through each host one at a time and run gobuster, save output to file
+for host in $(cat hosts); do
+    echo $host
+    gobuster dir -u $host -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -t 50 -o $host.gobuster
+done
+
+
 # fff made .headers and .body files, headers has the headers of the response and body has the body of the response
+
 aws s3 cp --recursive roots s3://mass-recon-warehouse/`basename $PWD`
 aws s3 cp hosts s3://mass-recon-warehouse/`basename $PWD`/
 aws s3 cp domains s3://mass-recon-warehouse/`basename $PWD`/
